152dccdc6c3e8b30a18865533df06f6622426732
Mykyta Tishkin <87891550+mykytatishkin@users.noreply.github.com>
2023-04-29 14:05:50 +0300

    /message fixed

--- cogs/message.py
+++ cogs/message.py
@@ -12,17 +12,14 @@ class MessageCommand(commands.Cog):
     
     @commands.has_permissions(administrator = True, manage_channels = True, manage_permissions = True, manage_messages = True)
     @commands.slash_command(description="You can send formatet message to some channel")
-    async def message(self, inter: disnake.ApplicationCommandInteraction, channel_id, title = "New message", your_message = "Your text here", color = 0x4287f5):
-        bot = commands.Bot(command_prefix = commands.when_mentioned)
-        channel = bot.get_channel(int(channel_id))
+    async def message(self, inter: disnake.ApplicationCommandInteraction, title = "New message", your_message = "Your text here", color = 0x4287f5):
         
-        await inter.response.send_message(f"Your message was sended, check it {channel}", )
         embed = disnake.Embed (
             title = f"{title}",
             description = f"{your_message}",
             color = color
             )
-        await channel.send(embed = embed)
+        await inter.response.send_message(embed = embed)
          
         
 
